804b62241a602b0a6d2303afda756d7df1759f94
Jordan Degner <jdegner0129@gmail.com>
2014-07-03 16:07:24 -0500

    Remove background script.

--- background.js
+++
@@ -1,9 +0,0 @@
-function checkPage(tabId) {
-    chrome.tabs.sendRequest(tabId, {}, undefined);
-}
-
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-    if (change.status == 'complete') {
-        checkPage(tabId);
-    }
-});
