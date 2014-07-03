cd9f591812520be5e14d98ad9681f4e5cb03976f
Jordan Degner <jdegner0129@gmail.com>
2014-07-03 16:12:09 -0500

    Clean up and fix nomination button placement.

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,6 @@
 $(function() {
     var wufooUrl = 'https://hudl.wufoo.com/forms/dev-sensei-nomination/def/field13=' + encodeURI(window.location.href);
+    var usersApiUrl = 'https://api.github.com/users/'
 
     var nominateButtonHtml = '<button type="button" class="minibutton nominate-button">Nominate!</button>';
     var nominateSidebarHtml = ' \
@@ -10,18 +11,18 @@ $(function() {
 
     var currentUser = $('#user-links').find('.name').text().trim();
 
-    $.get('https://api.github.com/users/' + currentUser, function(user) {
+    $.get(usersApiUrl + currentUser, function(user) {
         wufooUrl += '&field1=' + encodeURIComponent(user.email);
     });
 
     $('.discussion-sidebar').prepend(nominateSidebarHtml);
-    $('.timeline-comment:not(.timeline-comment-current-user) .timeline-comment-actions').prepend(nominateButtonHtml);
+    $('.timeline-comment:not(.timeline-comment-current-user):not(.timeline-comment-) .timeline-comment-actions').prepend(nominateButtonHtml);
     $('.nominate-button').click(function() {
         var parents = $(this).parents('.timeline-comment-header');
         if (parents && parents.length) {
             var nominee = parents.first().find('.author').text().trim();
 
-            $.get('https://api.github.com/users/' + nominee, function(user) {
+            $.get(usersApiUrl + nominee, function(user) {
                 if (user.email) {
                    wufooUrl += '&field2=' + encodeURIComponent(user.email); 
                 }
